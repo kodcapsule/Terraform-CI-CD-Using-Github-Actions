f89b5965d3035a392d807429dc21a1b1aaa9cb54
Kuseh <kusehsw@gmail.com>
2026-07-25 18:08:31 +0000

    remove commented-out instance_id output and clean up outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,6 @@ output "instance_public_ip" {
   value = module.devops-ec2.instance_public_ip
 }
 
-# output "instance_id" {
-#   value = module.devops-ec2.instance_id
-# }
 
 
 output "bucket_name" {
